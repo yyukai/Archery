4f73ba4da1d44b0710fb15f15a8cdb4deca80595
hhyo <rtttte@qq.com>
2018-09-28 20:26:14 +0800

    代码优化

--- sql/instance.py
+++ sql/instance.py
@@ -2,20 +2,17 @@
 import os
 import subprocess
 import time
-import datetime
 
 import simplejson as json
-from django.contrib.auth.decorators import permission_required
-from django.http import HttpResponse, QueryDict
-from django.views.decorators.csrf import csrf_exempt
-from django.db.models import Q
 from django.conf import settings
-from sql.utils.dao import Dao
-from sql.utils.group import user_instances
-from .models import Instance, DataBase, ParamTemp, ParamHistory, Replication
-from common.utils.extend_json_encoder import ExtendJSONEncoder
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
+
 from common.config import SysConfig
 from common.utils.aes_decryptor import Prpcrypt
+from common.utils.extend_json_encoder import ExtendJSONEncoder
+from sql.utils.dao import Dao
+from .models import Instance
 
 
 # 获取实例列表
@@ -24,24 +21,17 @@ def lists(request):
     limit = int(request.POST.get('limit'))
     offset = int(request.POST.get('offset'))
     type = request.POST.get('type')
-    db_type = request.POST.get('db_type')
     limit = offset + limit
+    search = request.POST.get('search', '')
 
-    # 获取搜索参数
-    search = request.POST.get('search')
-    if search is None:
-        search = ''
-    if type and db_type:
-        instances = Instance.objects.filter(instance_name__contains=search, type=type, db_type=db_type)
-    elif type:
-        instances = Instance.objects.filter(instance_name__contains=search, type=type)
-    elif db_type:
-        instances = Instance.objects.filter(instance_name__contains=search, db_type=db_type)
+    if type:
+        instances = Instance.objects.filter(instance_name__contains=search, type=type)[offset:limit] \
+            .values("id", "instance_name", "db_type", "type", "host", "port", "user")
+        count = Instance.objects.filter(instance_name__contains=search, type=type).count()
     else:
-        instances = Instance.objects.filter(instance_name__contains=search)
-
-    count = instances.count()
-    instances = instances[offset:limit].values("id", "instance_name", "db_type", "type", "host", "port", "user")
+        instances = Instance.objects.filter(instance_name__contains=search)[offset:limit] \
+            .values("id", "instance_name", "db_type", "type", "host", "port", "user")
+        count = Instance.objects.filter(instance_name__contains=search).count()
 
     # QuerySet 序列化
     rows = [row for row in instances]
@@ -56,21 +46,15 @@ def lists(request):
 def users(request):
     instance_id = request.POST.get('instance_id')
     instance_name = Instance.objects.get(id=instance_id).instance_name
-    # sql_get_user = '''select concat("\'", user, "\'", '@', "\'", host,"\'") as query from mysql.user;'''
-    search = request.POST.get('search', '')
-    if search:
-        sql_get_user = "SELECT user,host FROM mysql.user WHERE user like '%{0}%' or host like '%{0}%';".format(search)
-    else:
-        sql_get_user = "SELECT user,host FROM mysql.user;"
-    dao = Dao(instance_name=instance_name, db_name='mysql', flag=True)
+    sql_get_user = '''select concat("\'", user, "\'", '@', "\'", host,"\'") as query from mysql.user;'''
+    dao = Dao(instance_name=instance_name, flag=True)
     db_users = dao.mysql_query('mysql', sql_get_user)['rows']
     # 获取用户权限信息
     data = []
     for db_user in db_users:
         user_info = {}
-        user_priv = dao.mysql_query('mysql', "show grants for '{}'@'{}';".format(db_user[0], db_user[1]))['rows']
+        user_priv = dao.mysql_query('mysql', 'show grants for {};'.format(db_user[0]))['rows']
         user_info['user'] = db_user[0]
-        user_info['host'] = db_user[1]
         user_info['privileges'] = user_priv
         data.append(user_info)
     # 关闭连接
@@ -197,152 +181,3 @@ def getColumnNameList(request):
         result['status'] = 1
         result['msg'] = str(msg)
     return HttpResponse(json.dumps(result), content_type='application/json')
-
-
-@permission_required('sql.database', raise_exception=True)
-def db_list(request):
-    instance_name = request.POST.get('instance_name', '')
-    search = request.POST.get('search', '')
-    try:
-        obj_list = DataBase.objects.filter(instance_name=instance_name).filter(Q(db_name__contains=search) |
-                                                                               Q(db_application__contains=search) |
-                                                                               Q(db_person__contains=search))
-        res = list()
-        for obj in obj_list:
-            res.append({
-                'id': obj.id,
-                'db_name': obj.db_name,
-                'db_application': obj.db_application,
-                'db_person': obj.db_person,
-            })
-    except Instance.DoesNotExist:
-        print('Instance.DoesNotExist')
-    except Exception as e:
-        print(e)
-    return HttpResponse(json.dumps(res, cls=ExtendJSONEncoder, bigint_as_string=True),
-                        content_type='application/json')
-
-
-@csrf_exempt
-@permission_required('sql.menu_instance', raise_exception=True)
-def replication_delay(request):
-    """
-        instance_info = [
-            [ins1_id, ins1_ip, ins1],
-            [ins2_id, ins2_ip, ins2],
-            ......
-            [ins6_id, ins6_ip, ins6]
-        ]
-        delay_info = {
-            ins1_id: [[ins2, 0], [ins3, 0]],
-            ins5_id: [[ins6, 0]]
-        }
-        :param request:
-        :return:
-        """
-    masters = list()
-    delay_info = {}
-    all_instances = list()
-    for ins in user_instances(request.user, type='all', db_type='mysql'):
-        all_instances.append([str(ins.id), ins.host + ":" + str(ins.port), ins.instance_name])
-    hour = datetime.datetime.now() - datetime.timedelta(hours=1)
-    ins_name = request.GET.get('name', '')
-    if ins_name:
-        for ins in Instance.objects.filter(instance_name=ins_name):
-            masters.append([str(ins.id), ins.host + ":" + str(ins.port), ins.instance_name])
-    else:
-        for ins in user_instances(request.user, type='master', db_type='mysql'):
-            masters.append([str(ins.id), ins.host + ":" + str(ins.port), ins.instance_name])
-
-    for ins in user_instances(request.user, type='all', db_type='mysql'):
-        all_instances.append([str(ins.id), ins.host + ":" + str(ins.port), ins.instance_name])
-        slave_ins_info = list()
-        for slave in Instance.objects.filter(parent=ins, type='slave'):
-            rep = Replication.objects.filter(master=ins.instance_name, slave=slave.instance_name, created__gte=hour)
-            slave_ins_info.append([str(slave.id), rep[0].delay if rep else 9999])
-        delay_info[str(ins.id)] = slave_ins_info
-    res = {'instance_info': all_instances, 'masters': masters, 'delay_info': delay_info}
-    return HttpResponse(json.dumps(res, cls=ExtendJSONEncoder, bigint_as_string=True),
-                        content_type='application/json')
-
-
-@permission_required('sql.param_view', raise_exception=True)
-def param_list(request):
-    instance_name = request.POST.get('instance_name')
-    search = request.POST.get('search', '')
-    try:
-        ins = Instance.objects.get(instance_name=instance_name)
-        params = dict()
-        for p in ParamTemp.objects.filter(db_type=ins.db_type, param__contains=search):
-            params[p.param] = [p.default_var, p.is_reboot, p.available_var, p.description]
-        p_list = list(params.keys())
-        sql = "SELECT * FROM `information_schema`.`GLOBAL_VARIABLES` WHERE VARIABLE_NAME in ('{}');".format("','".join(p_list))
-        col_list = Dao(instance_name=instance_name).mysql_query('information_schema', sql)
-        print(sql, col_list)
-        res = list()
-        for idx, val in col_list['rows']:
-            res.append({
-                'p': idx,
-                'd_v': params[idx][0],
-                'rb': '是' if params[idx][1] == 1 else '否',
-                'a_v': params[idx][2],
-                'desc': params[idx][3],
-                'r_v': val
-            })
-    except Instance.DoesNotExist:
-        print('Instance.DoesNotExist')
-    except Exception as e:
-        print(e)
-    return HttpResponse(json.dumps(res, cls=ExtendJSONEncoder, bigint_as_string=True),
-                        content_type='application/json')
-
-
-@permission_required('sql.param_view', raise_exception=True)
-def param_history(request):
-    limit = int(request.POST.get('limit'))
-    offset = int(request.POST.get('offset'))
-    limit = offset + limit
-    instance_name = request.POST.get('instance_name')
-    search = request.POST.get('search', '')
-    if search:
-        phs = ParamHistory.objects.filter(instance__instance_name=instance_name)[offset:limit]
-    else:
-        phs = ParamHistory.objects.filter(instance__instance_name=instance_name, param__contains=search)[offset:limit]
-    res = list()
-    for r in phs:
-        is_active = '是' if r.is_active == 1 else '否'
-        res.append({"id": r.id, "p": r.param, "old_v": r.old_var, "new_v": r.new_var, "act": is_active, "t": r.create_time})
-    result = {'total': len(res), 'rows': res}
-    return HttpResponse(json.dumps(result, cls=ExtendJSONEncoder, bigint_as_string=True),
-                        content_type='application/json')
-
-
-@csrf_exempt
-@permission_required('sql.param_edit', raise_exception=True)
-def param_save(request):
-    post_data = QueryDict(request.body).dict()
-    instance_name = post_data['instance_name']
-    param = post_data['p']
-    run_v = post_data['r_v']
-
-    try:
-        sql = "SELECT * FROM `information_schema`.`GLOBAL_VARIABLES` WHERE VARIABLE_NAME='{}';".format(param)
-        col_list = Dao(instance_name=instance_name).mysql_query('information_schema', sql)
-        p = col_list['rows'][0]
-        if p[0] == run_v:
-            return HttpResponse(json.dumps({'status': 1, 'msg': '参数与实际一致，未调整！'}), content_type='application/json')
-        else:
-            ph = ParamHistory.objects.create(instance=Instance.objects.get(instance_name=instance_name), param=param,
-                                             old_var=p[1], new_var=run_v)
-            sql = "UPDATE GLOBAL_VARIABLES SET VARIABLE_VALUE ='{}' WHERE VARIABLE_NAME='{}';".format(run_v, param)
-            col_list = Dao(instance_name=instance_name).mysql_query('information_schema', sql)
-            if 'Error' in col_list:
-                ph.is_active = 1
-                res = {'status': 1, 'msg': col_list['Error']}
-            else:
-                ph.is_active = 0
-                res = {'status': 0, 'msg': '更新成功！'}
-            ph.save()
-    except Exception as e:
-        res = {'status': 1, 'msg': str(e)}
-    return HttpResponse(json.dumps(res), content_type='application/json')
